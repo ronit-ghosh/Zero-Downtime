1e19122a065d296648a1c55e4f0c4340da03c38f
ronit-ghosh <ronitghosh06@gmail.com>
2025-07-17 03:24:07 +0530

    feat: pusher added

---
+++ apps/pusher/index.ts
@@ -0,0 +1,30 @@
+import prisma from "@zero-downtime/db/client";
+import { xAddBulk } from "@zero-downtime/redis/client";
+
+async function pushWebsitesInStream() {
+  try {
+    const websites = await prisma.website.findMany({
+      select: {
+        id: true,
+        url: true,
+      },
+    });
+    if (websites.length === 0) {
+      throw new Error("[pusher]: Empty `websites` table!");
+    }
+    const response = await xAddBulk(websites);
+    console.log(
+      "added to the stream: " + response.results.map((r) => r) + "\n"
+    );
+    if (response.errors.length > 0)
+      console.error(response.errors.map((e) => e));
+    console.log(`${websites.length} websites pushed to redis stream`);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+setInterval(() => {
+  pushWebsitesInStream();
+}, 3 * 100);
